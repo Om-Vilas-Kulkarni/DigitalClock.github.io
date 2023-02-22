bddd0e4a69a822e2773a7fa70982cee260188b50
Om-Vilas-Kulkarni <kulkarniom-cmpn@atharvacoe.ac.in>
2023-02-22 22:51:55 +0530

    Updates

--- script.js
+++ script.js
@@ -22,6 +22,21 @@ let clock = new Date();
     let seconds = clock.getSeconds();
     
 
+    setInterval(() => {
+        d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+    
+    hours.style.transform =    `rotate(${hrotation}deg)`;
+    minutes.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`; 
+    }, 1000);
+
     function displayTime(){
         time = new Date();
         hr = time.getHours();
@@ -29,7 +44,6 @@ let clock = new Date();
         sec = time.getSeconds();
 
         clktime = hr+":"+min+":"+sec;
-        console.log(sec);
         document.getElementById("livetime").innerHTML = clktime;
     }
     setInterval(displayTime,1000);
